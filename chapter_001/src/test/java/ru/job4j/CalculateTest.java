9e58c763727abc940552743778b0221a8a2b8c10
Alexander Mezgin <nolich@gmail.com>
2016-11-15 19:04:01 +0300

    add maven, javaDoc and JUnit

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Alexander Mezgin 
+ * @version 1
+ * @since 14.11.2016
+ */
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenExecuteMainThenPrintToConsole() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World!\r\n"));
+    }
+}
--- src/main/java/ru/job4j/Calculate.java
+++
@@ -1,7 +0,0 @@
-package amezgin;
-
-public class Calculate {
-	public static void main (String[] args) {
-		System.out.println("Hello world!");
-	}
-}
