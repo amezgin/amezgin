00eb7eb75783cace899e8ebd83a5208c77b86d99
Alexander Mezgin <nolich@gmail.com>
2016-11-19 21:07:04 +0300

    Completed tasks 3.2 and 3.3

--- chapter_001/Conditional operator/max/src/main/java/ru/job4j/Max.java
+++ chapter_001/Conditional operator/max/src/main/java/ru/job4j/Max.java
@@ -1,1 +1,36 @@
 package ru.job4j;
+
+/**
+ * Class Max.
+ *
+ * @author Alexander Mezgin
+ * @version 1
+ * @since 17.11.2016
+ */
+public class Max {
+
+	/**
+	 *This method calculates the maximum value of the two arguments.
+	 *@param first It is first argument
+	 *@param second It is second argument
+	 *@return Return maximum value
+	 */
+	public int max(int first, int second) {
+		if (first > second) {
+			return first;
+		} else {
+			return second;
+		}
+	}
+
+	/**
+	 *This method calculates the maximum value of the Three arguments.
+	 *@param first It is first argument
+	 *@param second It is second argument
+	 *@param third It is third argument
+	 *@return Return maximal value
+	 */
+	public int max(int first, int second, int third) {
+		return max(max(first, second), third);
+	}
+}
--- chapter_001/Conditional operator/max/src/test/java/ru/job4j/MaxTest.java
+++ chapter_001/Conditional operator/max/src/test/java/ru/job4j/MaxTest.java
@@ -1,1 +1,63 @@
 package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class Max.
+ *
+ * @author Alexander Mezgin
+ * @version 1
+ * @since 18.11.2016
+ */
+public class MaxTest {
+    /**
+     * Test max with two arguments.
+     */
+    @Test
+    public void whenAddTwoValuesThenReturnMax() {
+        final Max maxValue = new Max();
+
+		final int checked = 3;
+		final int first = 2;
+		final int second = 3;
+
+		final int result = maxValue.max(first, second);
+
+        assertThat(result, is(checked));
+    }
+
+	/**
+     * Test max with three arguments.
+     */
+    @Test
+    public void whenAddTwoValuesThenReturnMax() {
+        final Max maxValue = new Max();
+
+		final int checked = 4;
+		final int first = 2;
+		final int second = 3;
+		final int third = 4;
+
+		final int result = maxValue.max(first, second, third);
+
+        assertThat(result, is(checked));
+    }
+
+	/**
+     * Test max with two arguments, when they are equal.
+     */
+    @Test
+    public void whenAddTwoValuesThenReturnMax() {
+        final Max maxValue = new Max();
+
+		final int checked = 3;
+		final int first = 3;
+		final int second = 3;
+
+		final int result = maxValue.max(first, second);
+
+        assertThat(result, is(checked));
+    }
+}
