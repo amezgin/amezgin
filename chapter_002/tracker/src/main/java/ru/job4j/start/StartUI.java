1c3cf21be954703cd90e215566f886fd38d33aa8
Alexander Mezgin <nolich@gmail.com>
2016-12-09 23:39:02 +0300

    Completed task 2.3. Added manifest in pom.xml and added class StartUI

---
+++ chapter_002/tracker/src/main/java/ru/job4j/start/StartUI.java
@@ -0,0 +1,17 @@
+package ru.job4j.start;
+
+/**
+ *The class StartUI.
+ * @author Alexander Mezgin
+ * @since 08.12.2016
+ * @version 1.0
+ */
+public class StartUI {
+    /**
+     *main.
+     *@param args args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
