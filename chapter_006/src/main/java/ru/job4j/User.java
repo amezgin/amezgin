1454b4461e368920a21ff8a3c411a43977be9023
Alexander Mezgin <nolich@gmail.com>
2017-10-09 21:53:37 +0300

    Демонстрация работы GC [#1589]

---
+++ chapter_006/src/main/java/ru/job4j/User.java
@@ -0,0 +1,84 @@
+package ru.job4j;
+
+/**
+ * Class User.
+ *
+ * @author Alexander Mezgin
+ * @version 1.0
+ * @since 08.10.2017
+ */
+public class User {
+    /**
+     * The name of user.
+     */
+    private String name;
+
+    /**
+     * The id of user.
+     */
+    private int id;
+
+    /**
+     * The constructor.
+     *
+     * @param name name of user.
+     * @param id id of user.
+     */
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    /**
+     * This method override the method finalize.
+     *
+     * @throws Throwable throwable.
+     */
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Finalize!");
+    }
+
+    /**
+     * This method override the method string.
+     *
+     * @return object representation in string line.
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    /**
+     * Tests for gc.
+     *
+     * @param args arguments from command line.
+     * @throws InterruptedException InterruptedException.
+     */
+    public static void main(String[] args) throws InterruptedException {
+        for (int index = 0; index < 400; index++) {
+            new User("name", index);
+        }
+        info();
+    }
+
+    /**
+     * This method print info about memory usage.
+     */
+    public static void info() {
+        int mb = 1024 * 1024;
+
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("##### Heap utilization statistics [MB] #####");
+        System.out.println("Used Memory:"
+                + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+        System.out.println("Free Memory:"
+                + runtime.freeMemory() / mb);
+        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+    }
+}
